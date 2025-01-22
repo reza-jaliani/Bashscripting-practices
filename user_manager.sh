085fadeeab7f93b2783d1941e34b29ca88ab15ec
Reza Jaliani <mrz.jaliani@gmail.com>
2025-01-22 18:32:42 +0330

    Create user_manager.sh
    V1-Creating menu

---
+++ user_manager.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#function AddUser {
+
+#}
+
+#function ListUser {
+
+#}
+
+#function DeleteUser{
+
+#}
+
+while :
+do
+        echo "*****-------User Manager Program-------*****"
+        echo "Use this program to manage users"
+        echo "Please use one of these options below:"
+        echo "1 --- To show list of users"
+        echo "2 --- To create a new user"
+        echo "3 --- To delete a user"
+        echo "0 --- To exit"
+        read option
+done
